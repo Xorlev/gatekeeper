3f7454e95bc7ec3386aef3b4e81c817b42e19f4f
Michael Rose <elementation@gmail.com>
2013-07-28 22:33:07 -0600

    NginxManager to reload nginx

---
+++ src/main/java/com/xorlev/gatekeeper/manager/NginxManager.java
@@ -0,0 +1,30 @@
+package com.xorlev.gatekeeper.manager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * cat /opt/nginx/logs/nginx.pid | sudo xargs -I{} kill -HUP {}
+ *
+ * @author Michael Rose <michael@fullcontact.com>
+ */
+public class NginxManager {
+    private static final Logger log = LoggerFactory.getLogger(NginxManager.class);
+
+    public static int reloadNginx(int pid) {
+        try {
+            return new ProcessBuilder().command(
+                    "sudo", "kill", "-HUP", Integer.toString(pid)
+            ).start().waitFor();
+        } catch (Exception e) {
+            log.error("Error attempting to restart NGINX: " + e.getMessage(), e);
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        NginxManager.reloadNginx(31935);
+    }
+}
